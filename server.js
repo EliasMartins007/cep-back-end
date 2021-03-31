aacb2a23ced8e87fe736c97cd2a65d742839733e
elias <eliasfaculdade@yahoo.com.br>
2021-03-31 11:22:18 -0300

    atualização cep

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.use(cors());
 
 app.listen(PORT, () => console.log(`servidor rodando na porta:${PORT}`));
 
+// app.get('/', (req, res) => {
+//   res.json({ message: 'OK' });
+// });
+
+// app.get('/', (req, res) => {
+//   const data = req.query;
+//   console.log(data);
+// });
+
 app.get('/', (req, res) => {
-  res.json('OK');
+  const { tracking } = req.query;
+  console.log(tracking);
 });
