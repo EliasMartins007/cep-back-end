2c062606ab949e972881f4c753d831ccbfcc118c
elias <eliasfaculdade@yahoo.com.br>
2021-03-31 14:40:52 -0300

    consultando

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Correios = require('node-correios');
+const correios = new Correios();
 const cors = require('cors');
 const app = express();
 const PORT = 3003;
@@ -16,7 +18,39 @@ app.listen(PORT, () => console.log(`servidor rodando na porta:${PORT}`));
 //   console.log(data);
 // });
 
+// app.get('/', (req, res) => {
+//   const { tracking } = req.query; //funciona basico sem o CEP 31/03/2021
+//   console.log(tracking);
+// });
+
+// correios   //fiz teste no back end funcionou 2021
+//   .consultaCEP({ cep: '33925520' })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
 app.get('/', (req, res) => {
   const { tracking } = req.query;
-  console.log(tracking);
+  // prettier-ignore
+  correios.consultaCEP({ cep: tracking })
+  .then(result =>{
+    res.send(result)
+    console.log(result);
+  })
+  .catch(error =>{
+    console.log(error);
+  });
 });
+//exemplo gomide
+// function executeDivisionPromise() {
+//   divisionPromise(12, 0)
+//     .then(result => {
+//       console.log(result);
+//     })
+//     .catch(errorMessage => {
+//       console.log('Falha na divisão ' + errorMessage);
+//     });
+// }
